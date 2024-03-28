022cfe8ba86602eb6951b59ddeb5e06de4bb9fcf
Mateus Santana <mateus-sgn@hotmail.com>
2024-03-28 16:40:30 -0300

    feat: Create Bill constants to make the code more readable

---
+++ api/src/main/java/com/bill/splitter/api/calculator/BillConstants.java
@@ -0,0 +1,13 @@
+package com.bill.splitter.api.calculator;
+
+public class BillConstants {
+
+    public static final String ADDITION_RATE = "additionRate";
+    public static final String CLIENTS_BILL = "clientsBill";
+    public static final String DELIVERY_FEE = "deliveryFee";
+    public static final String DISCOUNT_COUPON = "discountCoupon";
+    public static final String DISCOUNT_RATE = "discountRate";
+    public static final String BILLING_LINK = "https://picipay.me/mateussgn/";
+    public static final String PRODUCT = "product";
+
+}
