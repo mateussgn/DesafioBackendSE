36894caafc15772dc0e3e46e70597e394fc5956b
Mateus Santana <mateus-sgn@hotmail.com>
2024-03-28 16:33:38 -0300

    feat: Create order items types

---
+++ api/src/main/java/com/bill/splitter/api/order/item/OrderItemType.java
@@ -0,0 +1,16 @@
+package com.bill.splitter.api.order.item;
+
+public enum OrderItemType {
+
+    ADDITION_RATE,
+    DELIVERY_FEE,
+    DISCOUNT_COUPON,
+    DISCOUNT_RATE,
+    PRODUCT;
+}
+
+
+
+
+
+
