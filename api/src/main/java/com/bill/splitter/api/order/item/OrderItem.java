93ce9409aa4c67c6cb0c258f3d7f52b2992d3502
Mateus Santana <mateus-sgn@hotmail.com>
2024-03-28 16:44:25 -0300

    fix: Remove redundant constructor

--- api/src/main/java/com/bill/splitter/api/order/item/OrderItem.java
+++ api/src/main/java/com/bill/splitter/api/order/item/OrderItem.java
@@ -1,10 +1,4 @@
 package com.bill.splitter.api.order.item;
 
 public record OrderItem (OrderItemType orderItemType, Float value, String client) {
-
-    public OrderItem (OrderItemType orderItemType, Float value, String client) {
-        this.orderItemType = orderItemType;
-        this.value = value;
-        this.client = client;
-    }
 }
