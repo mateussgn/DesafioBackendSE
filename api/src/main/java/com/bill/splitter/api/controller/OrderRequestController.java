3ad27d4f81a0cc2baae70beacae688bdce191ba2
Mateus Santana <mateus-sgn@hotmail.com>
2024-03-28 16:33:17 -0300

    feat: Create GET endpoint

---
+++ api/src/main/java/com/bill/splitter/api/controller/OrderRequestController.java
@@ -0,0 +1,26 @@
+package com.bill.splitter.api.controller;
+
+import com.bill.splitter.api.calculator.Bill;
+import com.bill.splitter.api.calculator.BillImpl;
+import com.bill.splitter.api.order.item.OrderItem;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Map;
+
+@RestController
+@RequestMapping("orderRequest")
+public class OrderRequestController {
+
+    @GetMapping
+    public ResponseEntity<Map<String, String>> addOrderRequest (@RequestBody List<OrderItem> orderItemsDTO) {
+
+        Bill bill = new BillImpl();
+
+        return ResponseEntity.ok(bill.calculate(orderItemsDTO));
+    }
+}
